e6874a09c3b6f54a1f4aef4e2d93873cd07a4c7c
gts-bootcamp <52498440+gts-bootcamp@users.noreply.github.com>
2019-07-03 15:25:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-variable "name" { default = "VANDERBOON"}
+variable "name" { default = "CHANGE-ME"}
 provider "aws" { region  = "us-east-1" }
-data "aws_ami" "virginia-tsys-amzn-ami" {
+data "aws_ami" "ami" {
   most_recent = true
   owners = ["amazon"]
   filter { name = "name", values = ["amzn2-ami-hvm-2.0*x86*"] }
 }
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.virginia-tsys-amzn-ami.id}"
+  ami           = "${data.aws_ami.ami.id}"
   instance_type = "t2.micro"
   subnet_id = "subnet-0b1a60c5dd16fa6b5"
   tags { Name  = "${var.name}-CREATED-BY-JENKINS" }
